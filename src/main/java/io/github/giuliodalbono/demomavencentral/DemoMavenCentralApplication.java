4b4467ee469f5355a7292dadf5a52f666a8344b9
giulio <gdalbono@soldo.com>
2023-03-15 16:34:23 +0100

    initial commit

---
+++ src/main/java/io/github/giuliodalbono/demomavencentral/DemoMavenCentralApplication.java
@@ -0,0 +1,13 @@
+package io.github.giuliodalbono.demomavencentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMavenCentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMavenCentralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/giuliodalbono/demomavencentral/DemoMavenCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.giuliodalbono.demomavencentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMavenCentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
